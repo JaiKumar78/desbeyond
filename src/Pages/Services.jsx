fdc703d787b83fae5de6fa187ec06474802d115e
Jai Kumar D <jaikumard7871@gmail.com>
2023-11-26 19:20:29 +0530

    including services and fonts

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -2,8 +2,58 @@ import React from "react";
 
 const Services = () => {
     return (
-        <div>
-            Services!
+        <div className="services">
+            <h1>Services</h1>
+            <div className="mainservices">
+                <div>
+                    <img src="https://via.placeholder.com/350" alt="" />
+                </div>
+                <div>
+                    <img src="https://via.placeholder.com/350" alt="" />
+                </div>
+                <div>
+                    <img src="https://via.placeholder.com/350" alt="" />
+                </div>
+            </div>
+            <div className="subservices"> 
+                <div>
+                    <img src="https://via.placeholder.com/200" alt="" />
+                </div>
+                <div>
+                    <img src="https://via.placeholder.com/200" alt="" />
+                </div>
+                <div>
+                    <img src="https://via.placeholder.com/200" alt="" />
+                </div>
+                <div>
+                    <img src="https://via.placeholder.com/200" alt="" />
+                </div>
+                <div>
+                    <img src="https://via.placeholder.com/200" alt="" />
+                </div>
+                <div>
+                    <img src="https://via.placeholder.com/200" alt="" />
+                </div>
+                <div>
+                    <img src="https://via.placeholder.com/200" alt="" />
+                </div>
+                <div>
+                    <img src="https://via.placeholder.com/200" alt="" />
+                </div>
+                <div>
+                    <img src="https://via.placeholder.com/200" alt="" />
+                </div>
+            </div>
+            <div className="overview">
+                <div>
+                    <h3>Overview</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sitvoluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                </div>
+                <div>
+                    <h3>Approach</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sitvoluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                </div>
+            </div>
         </div>
     )
 }
